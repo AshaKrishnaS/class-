780078f5662d8d4840ec6ea72bdd55efef77f7b8
Asha Krishna S <68347359+AshaKrishnaS@users.noreply.github.com>
2021-11-18 10:41:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace class1
+{
+    class Program
+{
+    string name;
+    int age;
+
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        p.name = "anagha";
+        p.age = 32;
+        Console.WriteLine("name is " + p.name);
+        Console.WriteLine("age id " + p.age);
+
+    }
+}
+}
